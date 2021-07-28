e4122fe5d0cafa2816e30420122e2939d7035e0d
Pedro Moreira <pedrocmoreir97@gmail.com>
2021-07-28 08:53:06 -0300

    configs for png and svg, starting home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+
+import Logo from '../../assets/logo.svg'
+
 import {
     Container,
-    Title,
+    Header,
 } from './styles'
 
 export function Home() {
     return (
         <Container>
-            <Title>Home</Title>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            <Header>
+
+            </Header>
         </Container>
     )
 
