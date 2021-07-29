082810f43a7fa49267faed731cb6bed89c5a1e61
Pedro Moreira <pedrocmoreira97@gmail.com>
2021-07-29 13:49:24 -0300

    Header finished

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize'
 
 import Logo from '../../assets/logo.svg'
+import { Car } from '../../components/Car';
 
 import {
     Container,
     Header,
+    TotalCars,
+    HeaderContent,
 } from './styles'
 
 export function Home() {
@@ -14,10 +18,20 @@ export function Home() {
             <StatusBar
                 barStyle="light-content"
                 backgroundColor="transparent"
+                translucent
             />
             <Header>
-
+                <HeaderContent>
+                    <Logo
+                        width={RFValue(108)}
+                        height={RFValue(12)}
+                    />
+                    <TotalCars>
+                        Total de 12 carros
+                    </TotalCars>
+                </HeaderContent>
             </Header>
+            <Car />
         </Container>
     )
 
