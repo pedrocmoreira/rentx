78a14f210af66ba32d9ba56672228fdf413d38cd
Pedro Henrique Do Carmo Moreira <pedro.moreira@logisticag2l.com.br>
2021-08-06 13:27:07 -0300

    Initializing slider component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../ImageSlider';
 
 
 import * as S from './styles'
 
-export function CarDetails(){
-    return(
+export function CarDetails() {
+    return (
         <S.Container>
             <S.Header>
-                <BackButton onPress={() => {}}/>
+                <BackButton onPress={() => { }} />
             </S.Header>
+
+            <S.CarImages>
+                <ImageSlider
+                    imagesUrl={['https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png?fit=around%7C875:492.1875']}
+                />
+            </S.CarImages>
         </S.Container>
     )
 }
