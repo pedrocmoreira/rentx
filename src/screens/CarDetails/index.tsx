bf6c9e585a3f2e0178ae3defd3c84935b5c104f1
Pedro Henrique Do Carmo Moreira <pedro.moreira@logisticag2l.com.br>
2021-08-06 16:33:59 -0300

    Details interface

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -17,6 +17,23 @@ export function CarDetails() {
                     imagesUrl={['https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png?fit=around%7C875:492.1875']}
                 />
             </S.CarImages>
+
+            <S.Content>
+                <S.Details>
+                    <S.Description>
+                        <S.Brand>Lamborghini</S.Brand>
+                        <S.Name>Huracan</S.Name>
+                    </S.Description>
+                    <S.Rent>
+                        <S.Period>Ao dia</S.Period>
+                        <S.Price>R$ 580</S.Price>
+                    </S.Rent>
+                </S.Details>
+                <S.About>
+                    Este é um automóvel esportivo. Surgiu do lendário 
+                    touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+                </S.About>
+            </S.Content>
         </S.Container>
     )
 }
