9ae99956dfdff79ed5be2256b22346e316fa7302
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-04-24 23:02:11 -0300

    Add user gesture to home button

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -34,7 +34,7 @@ export function CarDetails() {
     const scrollY = useSharedValue(0);
     const scrollHandler = useAnimatedScrollHandler(event => {
         scrollY.value = event.contentOffset.y;
-        console.log(event.contentOffset.y);
+        // console.log(event.contentOffset.y);
     });
 
     const headerStyleAnimation = useAnimatedStyle(() => {
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://192.168.0.15:3000`,
+  baseURL: `http://SeuIP:3000`,
 });
 
 export { api };
